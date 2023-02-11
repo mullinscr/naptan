44c5ceda35051315f5db2df761209262f26e9e6c
callum-mullins <mullinscr@gmail.com>
2023-02-11 21:05:41 +0000

    Initial rewrite for v0.2.0
    Simplification of the package to return results as a pandas DataFrame instead.

--- naptan/naptan.py
+++ naptan/naptan.py
@@ -31,205 +31,22 @@ Examples
 >> filtered[0].to_dict()
 
 """
-import csv
-import datetime
-import re
-import requests
-from typing import Any, List, Dict, Optional, Iterable
+from io import BytesIO
+from typing import Iterable
 
-from attrs import define, field, asdict
+import requests
 import pandas as pd
+
 
 _BASE_URL = 'https://naptan.api.dft.gov.uk'
 _API_VERSION = 'v1'
 
+
 class APIError(Exception):
     """Raised for a status code != 200 returned from NaPTAN API"""
-    pass
 
 
-def _str_to_datetime(d_str: str) -> Optional[datetime.datetime]:
-    """
-    Converts a string representation of a datetime into a timezone aware
-    datetime.datetime obj.
-
-    Attempts to convert string conforming to ISO 8601 standard.
-
-    Parameters
-    ----------
-    str : str
-        The string to be converted to a datetime.datetime obj.
-
-    Returns
-    -------
-    datetime.datetime or None
-        Return a timezone-aware datetime.datetime obj if the conversion was
-        successful, otherwise None.
-    """
-    if not d_str or not isinstance(d_str, str):
-        return None
-
-    # checks for USO 8601 standard, but can't determine if date is valid
-    matcher = re.match(r'(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})\.?(\d{1,6})?\d*([+-]\d.*)?', d_str)
-    if not matcher:
-        return None
-    
-    year, month, day, hour, minute, second, microseconds, offset = matcher.groups()
-    if not microseconds:
-        microseconds = 0
-
-    if offset:
-        matcher = re.match(r'([+-])(\d{2}):?(\d{2}):?(\d{2})?', offset)
-        sign, hours, minutes, seconds = matcher.groups()
-        if not seconds:
-            seconds = 0
-        if not minutes:
-            minutes = 0
-
-        offset_seconds = int(seconds) + (int(minutes) * 60) + (int(hours) * 3600)
-        if sign == '-':
-            offset_seconds *= -1
-        tzinfo=datetime.timezone(datetime.timedelta(seconds=offset_seconds))
-    else:
-        tzinfo=datetime.timezone(datetime.timedelta(seconds=+0))
-
-    try:
-        converted_datetime = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute), int(second), int(microseconds), tzinfo=tzinfo)
-    except ValueError: # the string matched the ISO 8601 standard but wasn't a valid date
-        return None
-    return converted_datetime
-
-@define
-class Stop:
-    atco_code: str
-    naptan_code: str
-    plate_code: str
-    cleardown_code: str
-    common_name: str
-    common_name_lang: str
-    short_common_name: str
-    short_common_name_lang: str
-    landmark: str
-    landmark_lang: str
-    street: str
-    street_lang: str
-    crossing: str
-    crossing_lang: str
-    indicator: str
-    indicator_lang: str
-    bearing: str
-    nptg_locality_code: str
-    locality_name: str
-    parent_locality_name: str
-    grand_parent_locality_name: str
-    town: str
-    town_lang: str
-    suburb: str
-    suburb_lang: str
-    locality_centre: str
-    grid_type: str
-    easting: int = field(converter=int)
-    northing: int = field(converter=int)
-    longitude: float = field(converter=lambda x: float(x) if x else None)
-    latitude: float = field(converter=lambda x: float(x) if x else None)
-    stop_type: str
-    bus_stop_type: str
-    timing_status: str
-    default_wait_time: str
-    notes: str
-    notes_lang: str
-    administrative_area_code: int = field(converter=int)
-    creation_datetime: datetime.datetime = field(converter=lambda x: _str_to_datetime(x))
-    modification_datetime: datetime.datetime = field(converter=lambda x: _str_to_datetime(x))
-    revision_number: int = field(converter=lambda x: int(x) if x else None)
-    modification: str
-    status: str
-
-    def to_dict(self) -> Dict[str, Any]:
-        """Returns the Stop as a python dictionary, with attributes as keys and
-        the attribute values as values."""
-        return asdict(self)
-
-
-@define
-class StopList:
-    """
-    Holds a list of Stop objects.
-    """
-    _stops: List[Stop]
-
-    def __iter__(self):
-        return iter(self._stops)
-
-    def __getitem__(self, n):
-        return self._stops[n]
-
-    def __len__(self):
-        return len(self._stops)
-
-    def to_dict(self) -> Dict[str, Dict]:
-        """
-        Returns the StopList data as a dictionary with each Stop object's ATCO
-        code as the key.
-
-        Returns
-        -------
-        Dict[str, Dict]
-            The converted StopList.
-        """
-        return {stop.atco_code: asdict(stop) for stop in self}
-    
-    def to_dataframe(self) -> pd.DataFrame:
-        """
-        Returns the StopList as a pandas DataFrame.
-
-        Returns
-        -------
-        pd.DataFrame
-            The converted StopList.
-        """
-        return pd.DataFrame([asdict(stop) for stop in self])
-
-    def filter(
-            self,
-            stops: Optional[Iterable[str]] = None,
-            stop_type: Optional[Iterable[str]] = None,
-            status: Optional[Iterable[str]] = None
-        ) -> "StopList":
-        """
-        Returns a new StopList filtered via the `stops`, `stop_type` and `status`
-        keywords.
-
-        Parameters
-        ----------
-        stops : List[str], optional
-            List of stop ATCO codes as strings, by default None.
-            For example: ['1100DEB11527', '068000000757']
-        stop_type : List[str], optional
-            List of valid stop types as strings, by default None.
-            For example: ['BCT', 'AIR']
-        status : List[str], optional
-            List of valid stop status types as strings, by default None.
-            For example: ['active']
-
-        Returns
-        -------
-        StopList
-            The filtered StopList.
-            
-        """
-        stop_list = self
-        if stops:
-            stop_list = StopList([stop for stop in stop_list if stop.atco_code in stops])
-        if stop_type:
-            stop_list = StopList([stop for stop in stop_list if stop.stop_type in stop_type])
-        if status:
-            stop_list = StopList([stop for stop in stop_list if stop.status in status])
-        
-        return stop_list
-
-
-def _get_stops(area_codes: str = '') -> StopList:
+def _get_stops(area_codes: str = '') -> pd.DataFrame:
     """Process the API request and returned response."""
     response = _process_request(area_codes)
     return _process_response(response)
@@ -255,6 +72,7 @@ def _process_request(area_codes: str) -> requests.models.Response:
         code and reason.
     """
     response = requests.get(f'{_BASE_URL}/{_API_VERSION}/access-nodes',
+        timeout = 30,
         params = {
             'atcoAreaCodes': area_codes,
             'dataFormat': 'csv'
@@ -265,7 +83,7 @@ def _process_request(area_codes: str) -> requests.models.Response:
         raise APIError(response.status_code, response.reason)
     return response
 
-def _process_response(response: requests.models.Response) -> StopList:
+def _process_response(response: requests.models.Response) -> pd.DataFrame:
     """
     Process the valid returned API response and return a StopList object.
 
@@ -276,12 +94,13 @@ def _process_response(response: requests.models.Response) -> StopList:
 
     Returns
     -------
-    StopList
+    DataFrame
         All stops returned from valid response.
     """
-    csv_data = response.content.decode('utf-8').splitlines()
-    reader = csv.reader(csv_data[1:])
-    return StopList([Stop(*r) for r in reader])
+    return pd.read_csv(
+        BytesIO(response.content),
+        dtype={c: str for c in [1, 2, 6, 21, 22, 23, 24, 25]}
+    )
 
 def _format_stop_areas(stops: Iterable[str]) -> str:
     """
@@ -300,10 +119,10 @@ def _format_stop_areas(stops: Iterable[str]) -> str:
     str
         Formatted area code string for passing to the API request, eg. '260,080'
     """
-    areas = [stop[:3].rjust(3, '0') for stop in stops]
+    areas = list({stop[:3].rjust(3, '0') for stop in stops})
     return ','.join(areas)
 
-def get_specific_stops(stops: Iterable[str]) -> StopList:
+def get_specific_stops(stops: Iterable[str]) -> pd.DataFrame:
     """
     Returns a StopList containing just the specified stops, if present in the
     NaPTAN dataset.
@@ -316,14 +135,14 @@ def get_specific_stops(stops: Iterable[str]) -> StopList:
 
     Returns
     -------
-    StopList
+    DataFrame
         Containing just the specified stops.
     """
     stop_areas = _format_stop_areas(stops)
     returned_stops = _get_stops(stop_areas)
-    return returned_stops.filter(stops=stops)
+    return returned_stops.loc[returned_stops['ATCOCode'].isin(stops)]
 
-def get_area_stops(area_codes: Iterable[str]) -> StopList:
+def get_area_stops(area_codes: Iterable[str]) -> pd.DataFrame:
     """
     Returns a StopList containing all the stops that share the specified area codes.
 
@@ -334,19 +153,19 @@ def get_area_stops(area_codes: Iterable[str]) -> StopList:
 
     Returns
     -------
-    StopList
+    DataFrame
         Containing all available stops that share the specified area codes.
     """
     stop_areas = _format_stop_areas(area_codes)
     return _get_stops(stop_areas)
 
-def get_all_stops() -> StopList:
+def get_all_stops() -> pd.DataFrame:
     """
     Returns a StopList with all the available nationwide NaPTAN stops.
 
     Returns
     -------
-    StopList
+    DataFrame
         All available NaPTAN stops.
     """
     return _get_stops()
